3301d668038639d65e335b049c52cfa951282b79
YASWANTH <YASWANTH@192.168.11.32>
2019-10-29 16:58:33 +0530

    First Commit By Yaswanth Rangana

---
+++ onlinefoodordering/src/main/java/com/iiht/agnostic/hyd1/onlinefoodordering/OnlinefoodorderingApplication.java
@@ -0,0 +1,13 @@
+package com.iiht.agnostic.hyd1.onlinefoodordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinefoodorderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinefoodorderingApplication.class, args);
+	}
+
+}
---
+++ onlinefoodordering/src/test/java/com/iiht/agnostic/hyd1/onlinefoodordering/OnlinefoodorderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiht.agnostic.hyd1.onlinefoodordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinefoodorderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
